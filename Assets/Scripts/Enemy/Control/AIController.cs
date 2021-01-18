f3a25a95015fe50cf82a2e6b102c7fdece71a331
Akshit Sharma <waprajbhasha9@gmail.com>
2021-01-18 20:40:16 +0530

    enemy ai

--- Assets/Scripts/Enemy/Control/AIController.cs
+++ Assets/Scripts/Enemy/Control/AIController.cs
@@ -3,38 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-namespace Control
+namespace LoneWolfStudios.Control
 {
     public class AIController : MonoBehaviour
     {
-        [SerializeField] float chaseDistance = 5f;
-        NavMeshAgent Brain;
-        private void Awake()
+        public float wanderRadius;
+        public float wanderTimer;
+
+        private Transform target;
+        private NavMeshAgent agent;
+        private float timer;
+
+        private void Start()
         {
-            Brain = GetComponent<NavMeshAgent>();
+            agent = GetComponent<NavMeshAgent>();
+            timer = wanderTimer;
         }
         private void Update()
         {
-            var player = GameObject.FindWithTag("Player");
-            if (DistanceToPlayer(player) < chaseDistance)
+            timer += Time.deltaTime;
+
+            if (timer >= wanderTimer)
             {
-                Brain.SetDestination(player.transform.position);
-            }
-            else if(DistanceToPlayer(player) > chaseDistance)
-            {
-                Brain.SetDestination(transform.position);
+                Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+                agent.SetDestination(newPos);
+                timer = 0;
             }
         }
-
-        private float DistanceToPlayer(GameObject player)
+        public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
         {
-            return Vector3.Distance(player.transform.position, transform.position);
-        }
+            Vector3 randDirection = Random.insideUnitSphere * dist;
 
-        private void OnDrawGizmos()
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(transform.position, chaseDistance);
+            randDirection += origin;
+
+            NavMeshHit navHit;
+
+            NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+            return navHit.position;
         }
     }
 }
