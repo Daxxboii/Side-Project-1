69b7c1e67bdce2f946dc7c775da4a73623d63885
Akshit Sharma <waprajbhasha9@gmail.com>
2021-01-25 15:12:58 +0530

    hideing mechanism

--- Assets/Scripts/Player/Hide.cs
+++ Assets/Scripts/Player/Hide.cs
@@ -3,7 +3,43 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class Hide : MonoBehaviour
+namespace Scripts.Player
 {
-    
+    public class Hide : MonoBehaviour
+    {
+        GameObject hideButton;
+        [SerializeField] float range = 4f;
+        [SerializeField] LayerMask hidableLayer;
+
+        private void Awake()
+        {
+            hideButton = GameObject.Find("hide");
+            hideButton.SetActive(false);
+           
+        }
+
+        private void Update()
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, transform.forward, out hit, range, hidableLayer))
+                hideButton.SetActive(true);
+            else
+                hideButton.SetActive(false);
+
+        }
+        public void OnButtonPressed()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, range, hidableLayer))
+            {
+                if(hit.collider.tag == "Hideable")
+                {
+                    HidingController hc = hit.transform.gameObject.GetComponent<HidingController>();
+                    hc.GiveItToMe();
+                    
+                }
+                
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/hideMechanism/HidingController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HidingController : MonoBehaviour
+{
+    [SerializeField] GameObject cam, Unhide, player, FPSC;
+    private void Awake()
+    {
+        Unhide = GameObject.Find("unhide");
+        Unhide.SetActive(false);
+        cam = this.gameObject.transform.GetChild(0).gameObject;
+        player = GameObject.Find("Player");
+        FPSC = GameObject.Find("FpsCanvas");
+        
+    }
+
+    public void GiveItToMe()
+    {
+        player.SetActive(false);
+        Unhide.SetActive(true);
+        cam.SetActive(true);
+        FPSC.SetActive(false);
+    }
+
+    public void back()
+    {
+        player.SetActive(true);
+        Unhide.SetActive(false);
+        cam.SetActive(false);
+        FPSC.SetActive(true);
+    }
+}
