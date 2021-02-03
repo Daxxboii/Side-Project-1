6bf94bb2cb5e1d5613fef6b9cb3c3574c65a834a
Akshit Sharma <waprajbhasha9@gmail.com>
2021-02-03 13:18:51 +0530

    thunder

---
+++ Assets/Scripts/thunder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thunder : MonoBehaviour
+{
+    [SerializeField] float timer, timer1, timer2;
+    [SerializeField] AudioSource Thunder;
+    private void Awake()
+    {
+
+    }
+    private void Update()
+    {
+        timer += Time.time;
+        float T = Random.Range(timer1, timer2);
+        if(timer > T)
+        {
+            Debug.Log("1");
+            Thunder.Play();
+            timer = 0;
+        }
+    }
+}
