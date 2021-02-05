181749370924ff426339016c0de9b70ed2edc60e
Akshit Sharma <waprajbhasha9@gmail.com>
2021-02-05 14:22:10 +0530

    initilazitation

---
+++ Assets/Scripts/Misc/LightFliaker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using Random = UnityEngine.Random;
+
+public class LightFliaker : MonoBehaviour
+{
+    [SerializeField]
+    lightingVariables l = new lightingVariables();
+    Light lumin;
+    [Serializable]
+    struct lightingVariables
+    {
+        public float luminValStart, LuminValMax, r;
+        public bool isFlickering;
+    }
+    void Start()
+    {
+        lumin = GetComponent<Light>();
+    }
+    void Update()
+    {
+        if(l.isFlickering)
+        {
+        }
+
+        
+        else
+        {
+
+        }
+    }
+}
