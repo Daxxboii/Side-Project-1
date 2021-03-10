3ecf2aa988d5c30aad5735c4d2e64e25b67b36f1
Akshit Sharma <waprajbhasha9@gmail.com>
2021-03-10 14:11:16 +0530

    outline

--- Assets/Scripts/Misc/OutlineActive.cs
+++ Assets/Scripts/Misc/OutlineActive.cs
@@ -5,24 +5,35 @@ using UnityEngine;
 public class OutlineActive : MonoBehaviour
 {
     Outline o;
+    bool active;
     private void Awake()
     {
         o = gameObject.GetComponent<Outline>();
         o.enabled = false;
     }
-    
+    private void Update()
+    {
+        if(active)
+        {
+            o.enabled = true;
+        }
+        else
+        {
+            o.enabled = false;
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
-            o.enabled = true;
+            active = true;
         }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.tag == "Player")
         {
-            o.enabled = false;
+            active = false;
         }
     }
 }
