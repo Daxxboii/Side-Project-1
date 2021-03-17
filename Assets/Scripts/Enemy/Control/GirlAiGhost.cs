92a5ef67b7bb95bec815cda50176e1df127dda67
Daxxboii <64298070+Daxxboii@users.noreply.github.com>
2021-03-17 15:17:34 +0530

    Girl AI perfected

--- Assets/Scripts/Enemy/Control/GirlAiGhost.cs
+++ Assets/Scripts/Enemy/Control/GirlAiGhost.cs
@@ -19,44 +19,54 @@ namespace LoneWolfStudios.Control
        // [SerializeField]
         private float timer;
         private GameObject player;
-
+        [SerializeField]
+        private bool cooldown;
 
         [SerializeField]
-        public Animator Girl_animator;
-
-
+        private Animator Girl_animator;
+        [SerializeField]
+        private int No_of_hits,Hits_taken;
+        [SerializeField]
+        private float attack_distance;
+        [SerializeField]
+        private float Cooldown_period;
 
 
         private void Start()
         {
             agent = GetComponent<NavMeshAgent>();
             timer = wanderTimer;
+            Hits_taken = 0;
            //Tracker because gameobject being tracked by navmesh should be grounded
-
             player = GameObject.FindWithTag("Tracker");
+            cooldown = false;
         }
         private void Update()
         {
-            
-             
-            if (isinFrontOFMe(player))
+            if (!cooldown)
             {
-               // Debug.Log("chaising");
-                agent.SetDestination(player.transform.position);
-                Animations(2);
-            }
-            else if (!isinFrontOFMe(player))
-            {
-                //   Debug.Log("patoling");
-                Animations(1);
-                timer += Time.deltaTime;
 
-                if (timer >= wanderTimer)
+                if (isinFrontOFMe(player))
                 {
+                    // Debug.Log("chaising");
+                    agent.SetDestination(player.transform.position);
+                    Animations(2, 0);
+                    Attack();
+                }
 
-                    Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
-                    agent.SetDestination(newPos);
-                    timer = 0;
+                else if (!isinFrontOFMe(player))
+                {
+                    //   Debug.Log("patoling");
+                    Animations(1, 0);
+                    timer += Time.deltaTime;
+
+                    if (timer >= wanderTimer)
+                    {
+
+                        Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+                        agent.SetDestination(newPos);
+                        timer = 0;
+                    }
                 }
             }
         }
@@ -93,10 +103,38 @@ namespace LoneWolfStudios.Control
 
             return navHit.position;
         }
-
-        private void Animations(int walk_state)
+        /* walk state -1 is idle 
+         * walk state 2 is chasing
+         * walk state 1 is walking 
+         */
+        private void Animations(int walk_state , int hit_state)
         {
+            Girl_animator.SetInteger("Hit_State", hit_state);
             Girl_animator.SetInteger("Walk_State", walk_state);
+        }
+
+        private void Attack()
+        {
+            if (Hits_taken < No_of_hits)
+            {
+                if (Vector3.Distance(player.transform.position, transform.position) < attack_distance)
+                {
+                    cooldown = true;
+                    Hits_taken++;
+                    Animations(-1, 1);
+                    Invoke("ReActivate", Cooldown_period);
+                }
+            }
+            else
+            {
+                Animations(2, 2);
+              //  Debug.Log("kill;");
+            }
+        }
+
+        void ReActivate()
+        {
+            cooldown = false;
         }
     }
 }
