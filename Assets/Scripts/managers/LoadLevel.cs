766b6ba0f9e52ad3648823282c7d8c47431b3144
Akshit Sharma <waprajbhasha9@gmail.com>
2021-03-29 12:50:41 +0530

    Main Menu changes
    added progress bar

---
+++ Assets/Scripts/managers/LoadLevel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.WSA;
+using UnityEngine.UI;
+public class LoadLevel : MonoBehaviour
+{
+    public GameObject LoadingScreen;
+    public Slider slider;
+    public void loadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsyncronasly(sceneIndex));
+
+    }
+    IEnumerator LoadAsyncronasly( int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        LoadingScreen.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            slider.value = progress;
+
+            yield return null;
+
+        }
+    }
+}
