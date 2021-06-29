4ca6eb565c8057b318fc78f4318ae8923db0ccd5
Daxxboii <64298070+Daxxboii@users.noreply.github.com>
2021-06-29 13:31:38 +0530

    Optimisation 1

---
+++ Assets/Scripts/Testing/Static_collector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Static_collector : MonoBehaviour
+{
+    public Material[] light;
+    public MeshRenderer[] objs,lights;
+    int index;
+    void Start()
+    {
+        Collect();
+    }
+    void Collect()
+    {
+        objs = UnityEngine.Object.FindObjectsOfType<MeshRenderer>();
+
+        foreach (MeshRenderer go in objs)
+        {
+          
+            if (go.gameObject.activeInHierarchy)
+            {
+
+               if((go.material == light[0] || go.material == light[1]))
+                {
+                    lights[index] = go;
+                }
+            }
+            index++;
+        }
+    }
+}
