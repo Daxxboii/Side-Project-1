fa6e217f9a45954b09f8c3334debcdd19d484e50
Daxxboii <64298070+Daxxboii@users.noreply.github.com>
2021-07-04 14:05:39 +0530

    Button Tracking Done

--- Assets/Scripts/Player/ButtonOpen.cs
+++ Assets/Scripts/Player/ButtonOpen.cs
@@ -22,7 +22,9 @@ namespace Scripts.Buttons
         ObjectController oc;
         [SerializeField]
         Timeline_Manager tm;
-      
+        GameObject pointer;
+
+        public LayerMask interactables;
 
         public RaycastHit hit;
         void Awake()
@@ -49,82 +51,90 @@ namespace Scripts.Buttons
 
             if (Player.activeInHierarchy == true && Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 6f, lm))
             {
-                if (hit.transform.CompareTag("Door") || hit.transform.CompareTag("Escape Door") || hit.transform.CompareTag("Door_locker")|| hit.transform.CompareTag("Door_fence"))
-                {
-                    Intract.SetActive(true);
+              
+                    if (hit.transform.CompareTag("Door") || hit.transform.CompareTag("Escape Door") || hit.transform.CompareTag("Door_locker") || hit.transform.CompareTag("Door_fence"))
+                    {
+                        Intract.SetActive(true);
+                    Track(Intract);
+                    }
 
-                }
 
-               
-                if (hit.transform.CompareTag("Note"))
-                {
-                    note.SetActive(true);
+                    if (hit.transform.CompareTag("Note"))
+                    {
+                        note.SetActive(true);
+                    Track(note);
 
-                }
-                if (hit.transform.CompareTag("Save"))
-                {
-                    save.SetActive(true);
+                    }
+                    if (hit.transform.CompareTag("Save"))
+                    {
+                        save.SetActive(true);
+                    Track(note);
 
-                }
-                if (hit.transform.CompareTag("Hideable"))
-                {
-                    Hide.SetActive(true);
-                    Unhide.SetActive(false);
+                    }
+                    if (hit.transform.CompareTag("Hideable"))
+                    {
+                        Hide.SetActive(true);
+                        Unhide.SetActive(false);
+                     
 
-                }
-                if (hit.transform.CompareTag("pickup") || hit.transform.CompareTag("Doll")|| hit.transform.CompareTag("Fingie"))
-                {
-                    Pickup.SetActive(true);
-                    
-                   
-                }
-                if (hit.transform.CompareTag("Comic"))
-                {
-                    Comic.SetActive(true);
-                  
-                }
+                    }
+                    if (hit.transform.CompareTag("pickup") || hit.transform.CompareTag("Doll") || hit.transform.CompareTag("Fingie"))
+                    {
+                        Pickup.SetActive(true);
+                    Track(Pickup);
 
-                if (hit.transform.CompareTag("Map"))
-                {
-                    map.SetActive(true);
-                   comment.text = "Should take it , would be of help later";
-                }
 
-                if ((hit.transform.CompareTag("Timeline") && oc.had != null) || hit.transform.CompareTag("Timeline_independent"))
-                {
-                    Timeline.SetActive(true);
-                    comment.text = "Interact?";
-                }
-                 
-                if ((hit.transform.CompareTag("Timeline") && oc.had == null))
-                {
-                    comment.text = "get something ";
-                }
+                    }
+                    if (hit.transform.CompareTag("Comic"))
+                    {
+                        Comic.SetActive(true);
+                    Track(Comic);
 
-                if (hit.transform.CompareTag("Obstacle"))
-                {
-                    comment.text = "Going there wont be a good idea";
-                }
-                if (hit.transform.CompareTag("Blackboard"))
-                {
-                    comment.text = "Find something to write with";
-                }
-                if (hit.transform.CompareTag("Untagged"))
-                {
-                    Intract.SetActive(false);
-                    Pickup.SetActive(false);
-                    Drop.SetActive(false);
-                    Unhide.SetActive(false);
-                    save.SetActive(false);
-                    Hide.SetActive(false);
-                    Timeline.SetActive(false);
-                    note.SetActive(false);
-                    Comic.SetActive(false);
-                    map.SetActive(false);
-                    comment.text = "";
-                }
+                    }
+
+                    if (hit.transform.CompareTag("Map"))
+                    {
+                        map.SetActive(true);
+                        comment.text = "Should take it , would be of help later";
+                    Track(map);
+                    }
+
+                    if ((hit.transform.CompareTag("Timeline") && oc.had != null) || hit.transform.CompareTag("Timeline_independent"))
+                    {
+                        Timeline.SetActive(true);
+                        comment.text = "Interact?";
+                    Track(Timeline);
+                    }
+
+                    if ((hit.transform.CompareTag("Timeline") && oc.had == null))
+                    {
+                        comment.text = "get something ";
+                    }
+
+                    if (hit.transform.CompareTag("Obstacle"))
+                    {
+                        comment.text = "Going there wont be a good idea";
+                    }
+                    if (hit.transform.CompareTag("Blackboard"))
+                    {
+                        comment.text = "Find something to write with";
+                    }
+                    if (hit.transform.CompareTag("Untagged"))
+                    {
+                        Intract.SetActive(false);
+                        Pickup.SetActive(false);
+                        Drop.SetActive(false);
+                        Unhide.SetActive(false);
+                        save.SetActive(false);
+                        Hide.SetActive(false);
+                        Timeline.SetActive(false);
+                        note.SetActive(false);
+                        Comic.SetActive(false);
+                        map.SetActive(false);
+                        comment.text = "";
+                    }
+
                 
-
             }
             else
             {
@@ -168,6 +178,21 @@ namespace Scripts.Buttons
             Comic.SetActive(false);
             map.SetActive(false);
             comment.text = "";
+        }
+        void Track(GameObject Button)
+        {
+            foreach (Transform child in hit.transform)
+            {
+                if (child.tag == "Tracker")
+                {
+                     pointer = child.gameObject;
+                }
+                   
+            }
+            Vector3 pos = Camera.main.WorldToScreenPoint(pointer.transform.position);
+            Button.transform.position = pos;
+          
+            
         }
        
     }
